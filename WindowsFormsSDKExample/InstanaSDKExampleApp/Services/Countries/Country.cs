8fae03d1cf76fd6e683f53945e41dd25184e8ee4
Kristof Berger <kristof.berger@instana.com>
2019-06-26 11:00:28 +0200

    Change foler name for Windows example

---
+++ WindowsFormsSDKExample/InstanaSDKExampleApp/Services/Countries/Country.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace InstanaSDKExampleApp.Services.Countries
+{
+    public class Country
+    {
+
+        [JsonProperty("region")]
+            public string Region
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("name")]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("alpha3code")]
+        public string Code
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("capital")]
+        public string Capital
+        {
+            get;
+            set;
+        }
+        [JsonProperty("population")]
+        public long Population
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ WindowsFormsSDKExample/InstanaSDKExampleApp/Services/Countries/CountryService.cs
@@ -0,0 +1,57 @@
+﻿
+using Instana.ManagedTracing.Sdk.Spans;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace InstanaSDKExampleApp.Services.Countries
+{
+    public class CountryService
+    {
+        private HttpClient _client;
+        public CountryService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public ServiceResult<List<Country>> GetCountriesByRegion(string region)
+        {
+            // we do NOT explicitly create a span here. HttpClient will be autoinstrumented
+            // and thus we will see how long the call took.
+            ServiceResult<List<Country>> serviceResult = new ServiceResult<List<Country>>();
+            try
+            {
+                string byRegionUrl = $"https://restcountries.eu/rest/v2/region/{region.ToLower()}";
+                var result = _client.GetAsync(byRegionUrl).Result;
+                serviceResult.StatusCode = (int)result.StatusCode;
+                if (result.IsSuccessStatusCode)
+                {
+                    serviceResult.Result = TransformApiResultToCountryList(result);
+                }
+
+            }
+            catch(Exception e)
+            {
+                serviceResult.CallException = e;
+            }
+
+            return serviceResult;
+
+        }
+
+        private List<Country> TransformApiResultToCountryList(HttpResponseMessage response)
+        {
+            // let's create an intermediate span, which will show us how much the
+            // transformation-logic  contributes to the overall runtime.
+            using (var span = CustomSpan.Create(this, SpanType.INTERMEDIATE))
+            {
+                HttpContent content = response.Content;
+                var decoded = content.ReadAsStringAsync().Result;
+                var countries = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Country>>(decoded);
+                span.SetData("count", countries.Count.ToString());
+                return countries;
+            }
+        }
+    }
+}
