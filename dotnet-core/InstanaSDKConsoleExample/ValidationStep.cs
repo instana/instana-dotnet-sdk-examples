f5d8d5c76be56b971906e2d9d850291d6286ecf2
Kristof Berger <kristof.berger@instana.com>
2020-04-14 13:06:10 +0200

    Reorganize folders (dotnet-full-framework and dotnet-core), move examples,. start .NET Core docs

---
+++ dotnet-core/InstanaSDKConsoleExample/ValidationStep.cs
@@ -0,0 +1,27 @@
+﻿namespace InstanaSDKConsoleExample
+{
+    using Instana.Tracing.Sdk.Spans;
+    using System;
+    using System.Collections.Generic;
+
+    internal class ValidationStep : IStep
+    {
+        public string Execute(string input, Dictionary<string, object> protocol)
+        {
+            using (var span = CustomSpan.CreateEntry(this, CorrelationUtil.GetCorrelationFromCurrentContext))
+            {
+                span.WrapAction(() =>
+                {
+                    span.SetTag(new string[] { "Input" }, input);
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        protocol["Validation-Result"] = "Invalid";
+                        throw new ArgumentException("The input for this job was invalid!");
+                    }
+                    protocol["Validation-Result"] =  "Valid";
+                }, true);
+                return input;
+            }
+        }
+    }
+}
---
+++ dotnet-core/InstanaSDKConsoleExample/WordcountStep.cs
@@ -0,0 +1,31 @@
+﻿
+namespace InstanaSDKConsoleExample
+{
+    using Instana.Tracing.Sdk.Spans;
+    using System.Collections.Generic;
+
+    internal class WordcountStep : IStep
+    {
+        public string Execute(string input, Dictionary<string, object> protocol)
+        {
+            using (var span = CustomSpan.CreateEntry(this, CorrelationUtil.GetCorrelationFromCurrentContext))
+            {
+                span.WrapAction(() =>
+                {
+                    span.SetTag(new string[] { "Input" }, input);
+                    string[] words = input.Split(new char[] { ',', ' ', '!', '.', '?' });
+                    int icount = 0;
+                    foreach (string word in words)
+                    {
+                        if (!string.IsNullOrEmpty(word))
+                        {
+                            icount++;
+                        }
+                    }
+                    protocol["Word-Count"] = icount;
+                }, true);
+            }
+            return input;
+        }
+    }
+}
